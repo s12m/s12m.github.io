7bc73dd0076edfbaf7d85822d3abc73837dfe0ff
SAITO Kazuya <saito@smartalgorithm.co.jp>
2022-07-31 18:58:48 +0900

    Remove gatsby-plugin-typegen

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,28 +1,28 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'new',
+    siteUrl: 'https://www.yourdomain.tld'
   },
-  plugins: ['gatsby-plugin-postcss', "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sitemap', {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/logo/cube.png"
+      'icon': 'src/images/logo/cube.png'
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
+      'name': 'images',
+      'path': './src/images/'
     },
   }, 'gatsby-transformer-yaml', {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "data",
-      "path": "./src/data/"
+      'name': 'data',
+      'path': './src/data/'
     },
-  }, 'gatsby-plugin-typegen']
-};
+  }]
+}
 
-export default config;
+export default config
